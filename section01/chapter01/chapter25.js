1d8e5e6f7bd0aab4e6d6931d9ea20fb0443daa4d
ohjh9701 <ohjh4875@gmail.com>
2026-02-05 09:29:48 +0900

    [추가] 구조 분해 할당 추가 내용

--- section01/chapter01/chapter25.js
+++ section01/chapter01/chapter25.js
@@ -32,3 +32,13 @@ let {
 
 console.log(myAge, myName, myHobby, extra);
 
+//3.함수(람다식 = 화살표함수)
+//각 멤버변수를 변수로 저장하고 싶고 출력하고 싶다
+let printPerson = ({name, age, hobby, extrq = "Do it!"}) => {
+  console.log(`name = ${name}`);
+  console.log(`age = ${age + 1}`);
+  console.log(`hobby = ${hobby}`);
+  console.log(`extra = ${extra}`);
+}
+
+printPerson(person);
