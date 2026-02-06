ddd9f6c9d0ad138bab48fd646f2131bef9dc95a5
ohjh9701 <ohjh4875@gmail.com>
2026-02-06 09:30:54 +0900

    [생성] 리액트 시작

---
+++ section03/src/math.js
@@ -0,0 +1,14 @@
+//math 모듈 (객체: function - 함수선언식, 함수표현식, 람다식)
+//java = class
+
+export default function add(a, b) {
+  return a+b;
+}
+
+export function sub(a, b) {
+  return a-b;
+}
+
+export function div(a,b) {
+  return a/b;
+}
