76dc8d4dedc90b0c2e5a665926c5ba91580250cb
ohjh9701 <ohjh4875@gmail.com>
2026-02-10 09:56:03 +0900

    [생성] To Do List 실습을 위한 프로젝트 생성

---
+++ section08/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <h1>오늘의 할 일 APP</h1>
+      <div className="header"></div>
+      <div className="editor"></div>
+      <div className="list"></div>
+    </div>
+    </>
+  )
+}
+
+export default App
