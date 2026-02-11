ab6c2b6ed5641cb84b831c5e5e537f99e5be7506
ohjh9701 <ohjh4875@gmail.com>
2026-02-11 14:10:27 +0900

    [생성] useMemo Hooks 테스트

---
+++ section12/src/App.jsx
@@ -0,0 +1,85 @@
+import './css/App.css'
+import Editor from './components/Editor'
+import Header from './components/Header'
+import List from './components/List'
+import { useState, useRef, useReducer } from 'react';
+import Exam from './components/Exam';
+
+const mockData = [
+ {
+ id: 0,
+ isDone: false,
+ content: "React 공부하기",
+ date: new Date().getTime(),
+ },
+ {
+ id: 1,
+ isDone: false,
+ content: "SpringBoot 공부하기",
+ date: new Date().getTime(),
+ },
+ {
+ id: 2,
+ isDone: false,
+ content: "Oracle DB 공부하기",
+ date: new Date().getTime(),
+ },
+];
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data , ...todos];
+    case "UPDATE":
+      return todos.map((todo)=>{
+        return todo.id === action.id ?  {...todo, isDone:!todo.isDone} : todo
+      })
+    case "DELETE":
+      return todos.filter((todo)=>{
+      return todo.id !== action.id
+    })
+  }
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  //이벤트 함수(setTodos 생성)
+  const onCreate = (content) => {
+      let newTodo = {
+          id: idRef.current++,
+          isDone: false,
+          content: content,
+          date: new Date().getTime(),
+      }
+      dispatch({type:"CREATE", data : newTodo });
+  };
+
+  //이벤트 함수(setTodos 데이타 수정기능)
+  const onUpdate = (id)=>{
+    dispatch({
+      type:"UPDATE",
+      id:id
+    })
+  }
+  //이벤트 함수(setTodos 데이타 삭제기능)
+  const onDelete = (id) => {
+    dispatch({
+      type:"DELETE",
+      id:id
+    })
+  }
+  return (
+    <>
+    <div className="App">
+      <Header />
+      <Exam />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ section12/src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import '../css/TodoItem.css';
+
+const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
+  const onChangeIsDone = ()=> {
+    onUpdate(id);
+  }
+
+  const onClickDelete = ()=> {
+    onDelete(id);
+  }
+
+ return (
+ <div className="TodoItem">
+ <input onChange={onChangeIsDone} type="checkbox" checked={isDone}/>
+ <div className="content">{content}</div>
+ <div className="date">{new Date(date).toLocaleDateString()}</div>
+ <button onClick={onClickDelete}>삭제</button>
+ </div>
+ );
+};
+
+export default TodoItem;
