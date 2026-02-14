b82870a1423183afba4c017c62d387007cbe3da0
ohjh9701 <ohjh4875@gmail.com>
2026-02-14 16:00:22 +0900

    [수정] 수정하기 진행 후 디테일 페이지로 다시 가도록 수정

--- boardTest/src/components/Edit.jsx
+++ boardTest/src/components/Edit.jsx
@@ -22,8 +22,9 @@ const Edit = ()=>{
     }
     //no, title, writer, content, createdDate
     const onClickUpdateBoard = ()=>{
+        window.alert("입력하신 내용으로 수정하시겠습니까?");
         onUpdate(input.no, input.title, input.writer, input.content, input.createdDate)
-        nav("/",{replace:true})
+        nav((`/BoardDetail/${no}`),{replace:true})
     }
 
 
@@ -31,9 +32,6 @@ const Edit = ()=>{
         <div className="edit_board">
             <div className="detail-info">
           <span className="info-no">게시글 번호: {input.no}</span>
-          <span className="info-date">
-            작성일: {new Date(input.createdDate).toLocaleDateString()}
-          </span>
             <div className="input-group">
       <label>제목</label>
       <input name="title" value={input.title} onChange={onChangeBoardData} type="text"/>
@@ -43,7 +41,7 @@ const Edit = ()=>{
       <textarea name="content" value={input.content} onChange={onChangeBoardData}  rows="10"></textarea>
     </div>
     <div className="form-actions">
-      <button type="button" className="cancel-btn">수정취소</button>
+      <button type="button" className="cancel-btn" onClick={()=>nav(-1)}>수정취소</button>
       <button type="button" className="submit-btn" onClick={onClickUpdateBoard}>수정하기</button>
     </div>
         </div>
